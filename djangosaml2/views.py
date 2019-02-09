4de1fb84b7b959d44878e359b83497454d5acdc9
Chuck Mobley <chuck@vndly.com>
2019-02-08 22:10:38 -0500

    Adds more logging

--- djangosaml2/views.py
+++ djangosaml2/views.py
@@ -217,6 +217,7 @@ def login(request,
                     saml_request = base64.b64encode(binary_type(request_xml))
 
                 logger.debug("render")
+                logger.debug("post_binding_form_template")
                 http_response = render(request, post_binding_form_template, {
                     'target_url': location,
                     'params': {
